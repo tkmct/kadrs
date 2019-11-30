16698ce2804cfab2a4c142023cda61a8dba09544
tkmct <1220t.takamichi@gmail.com>
2019-11-30 22:02:04 +0900

    key use 160 bit

--- src/key.rs
+++ src/key.rs
@@ -1,35 +1,41 @@
-use byteorder::{BigEndian, ByteOrder};
 use ring::digest::{digest, SHA256};
 
-/// Key struct of hash table. `inner` field represents key hash.
-/// given string would be hashed with SHA256 and truncated into 32 bits.
-#[derive(Eq, PartialEq, Hash, Clone)]
-pub struct Key {
-    inner: u32,
-}
+/// Key struct represents Key of (Key, Value) pair and ID of nodes.
+/// id and key are represented as 160-bit identifier.
+/// distance between two keys are calcuated using XOR.
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+pub struct Key([u8; 20]);
 
 impl Key {
-    pub fn new(s: String) -> Self {
-        let hashed = digest(&SHA256, s.as_ref());
-        Self {
-            inner: BigEndian::read_u32(&hashed.as_ref()[0..4]),
-        }
+    pub fn new(k: [u8; 20]) -> Self {
+        Self(k)
     }
 
-    pub fn distance(&self, lhs: &Key) -> u32 {
-        self.inner ^ lhs.inner
+    pub fn distance(&self, rhs: &Key) -> [u8; 20] {
+        let xor: Vec<u8> = self
+            .0
+            .iter()
+            .zip(rhs.0.iter())
+            .map(|(l, r)| l ^ r)
+            .collect();
+        let mut arr = [0; 20];
+        arr.copy_from_slice(&xor[0..20]);
+        arr
     }
 }
 
 impl From<String> for Key {
     fn from(s: String) -> Self {
-        Self::new(s)
+        let hashed = digest(&SHA256, s.as_ref());
+        let mut arr = [0; 20];
+        arr.copy_from_slice(&hashed.as_ref()[0..20]);
+        Self(arr)
     }
 }
 
 impl From<&str> for Key {
     fn from(s: &str) -> Self {
-        Self::new(s.to_owned())
+        s.to_owned().into()
     }
 }
 
@@ -39,10 +45,10 @@ mod tests {
 
     #[test]
     fn test_key_distance() {
-        let key1: Key = "k1".into();
-        let key2: Key = "k2".into();
+        let key1 = Key::new([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        let key2 = Key::new([1, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         let d = key1.distance(&key2);
 
-        assert_eq!(d, 1810272128);
+        assert_eq!(d, key2.0);
     }
 }
