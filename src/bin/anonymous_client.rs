1f40b267d2083f167ba969403008c5389544beb1
tkmct <1220t.takamichi@gmail.com>
2019-12-09 02:35:27 +0900

    feat(client) parse cli input

--- src/bin/anonymous_client.rs
+++ src/bin/anonymous_client.rs
@@ -1,25 +1,61 @@
-use clap::{App, Arg, SubCommand};
+use {
+    clap::{App, Arg, ArgMatches, SubCommand},
+    kadrs::rpc::Rpc,
+    std::net::SocketAddrV4,
+};
+
+fn parse_method(matches: ArgMatches) -> Result<Rpc, ()> {
+    if let Some(_) = matches.subcommand_matches("ping") {
+        return Ok(Rpc::Ping);
+    }
+
+    if let Some(sub_match) = matches.subcommand_matches("find_value") {
+        let key = sub_match.value_of("key").ok_or(())?;
+        return Ok(Rpc::FindValue(key.into()));
+    }
+
+    if let Some(sub_match) = matches.subcommand_matches("find_node") {
+        let id = sub_match.value_of("id").ok_or(())?;
+        return Ok(Rpc::FindNode(id.into()));
+    }
+
+    if let Some(sub_match) = matches.subcommand_matches("store") {
+        let key = sub_match.value_of("key").ok_or(())?;
+        let value = sub_match.value_of("value").ok_or(())?;
+        return Ok(Rpc::Store(key.into(), value.into()));
+    }
+
+    Err(())
+}
 
 #[async_std::main]
-async fn main() {
+async fn main() -> Result<(), ()> {
     let app = App::new("kadrs-client")
         .version("0.1.0")
         .about("client app for kadrs")
+        .arg(Arg::with_name("host").required(true))
         .subcommands(vec![
             SubCommand::with_name("ping").about("PING to check if node with given host is alive"),
             SubCommand::with_name("find_value")
                 .about("FIND_VALUE to get stored value")
-                .args(&vec![
-                    Arg::with_name("host").help("host name. ex) 127.0.0.1:8080"),
-                    Arg::with_name("key"),
-                ]),
+                .arg(Arg::with_name("key").required(true)),
             SubCommand::with_name("find_node")
                 .about("FIND_NODE to get host of given id")
-                .arg(Arg::with_name("id")),
+                .arg(Arg::with_name("id").required(true)),
             SubCommand::with_name("store")
                 .about("STORE given key value pair")
-                .args(&vec![Arg::with_name("key"), Arg::with_name("value")]),
+                .args(&vec![
+                    Arg::with_name("key").required(true),
+                    Arg::with_name("value").required(true),
+                ]),
         ]);
     let matches = app.get_matches();
-    println!("{:?}", matches);
+    let host: SocketAddrV4 = match matches.value_of("host").unwrap().parse() {
+        Ok(addr) => addr,
+        Err(_) => panic!("Invalid host string"),
+    };
+    let rpc = parse_method(matches)?;
+    println!("request to host: {:?} {:?}", host, rpc);
+
+    Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod bucket;
+pub mod error;
+pub mod in_memory_hash_table;
+pub mod key;
+pub mod node;
+pub mod request;
+pub mod rpc;
