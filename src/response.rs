9322f60d7ace4a27cf8b938971699093a0c40406
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-12-27 11:45:22 +0900

    feat add body to response

--- src/response.rs
+++ src/response.rs
@@ -1,22 +1,33 @@
 use {
     crate::{node::NodeInfo, request::Request, rpc::Rpc},
+    arrayvec::ArrayVec,
     serde::{Deserialize, Serialize},
 };
+
+const K: usize = 10;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ResponseBody {
+    PONG,
+    VALUE(Vec<u8>),
+    NODES(Vec<NodeInfo>),
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Response {
     request_rpc: Rpc,
     from: NodeInfo,
     to: Option<NodeInfo>,
-    //TODO: add body
+    body: Option<ResponseBody>,
 }
 
 impl Response {
-    pub fn new(from: NodeInfo, to: Option<NodeInfo>, rpc: Rpc) -> Self {
+    pub fn new(from: NodeInfo, to: Option<NodeInfo>, rpc: Rpc, body: Option<ResponseBody>) -> Self {
         Self {
             from,
             to,
             request_rpc: rpc,
+            body,
         }
     }
 
@@ -32,11 +43,20 @@ impl Response {
         self.to.as_ref()
     }
 
+    pub fn get_body(&self) -> Option<&ResponseBody> {
+        self.body.as_ref()
+    }
+
+    pub fn set_body(&mut self, body: Option<ResponseBody>) {
+        self.body = body
+    }
+
     pub fn from_request(req: Request) -> Self {
         Self {
             from: req.get_to().clone(),
             to: req.get_from().map(|f| f.clone()),
             request_rpc: req.get_rpc().clone(),
+            body: None,
         }
     }
 }
