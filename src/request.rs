7b29a8c13b4490db13dff9ae687c2762825b7c30
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-12-27 18:50:14 +0900

    feat return Response

--- src/request.rs
+++ src/request.rs
@@ -1,5 +1,5 @@
 use {
-    crate::{error::Result, node::NodeInfo, rpc::Rpc},
+    crate::{error::Result, node::NodeInfo, response::Response, rpc::Rpc},
     async_std::{net::TcpStream, prelude::*},
     serde::{Deserialize, Serialize},
     std::net::Shutdown,
@@ -29,15 +29,16 @@ impl Request {
         &self.to
     }
 
-    pub async fn send(&self) -> Result<String> {
+    pub async fn send(&self) -> Result<Response> {
         let req_str = serde_json::to_string(&self)?;
         let mut stream = TcpStream::connect(self.to.get_host()).await?;
         stream.write_all(req_str.as_bytes()).await?;
         stream.write("\n".as_bytes()).await?;
         let mut buf = vec![0u8; 32];
         let count = stream.read(&mut buf).await?;
-        let res = String::from_utf8(buf[..count].to_vec())?.trim().to_owned();
         stream.shutdown(Shutdown::Both)?;
+        let res_str = String::from_utf8(buf[..count].to_vec())?.trim().to_owned();
+        let res: Response = serde_json::from_str(&res_str)?;
         Ok(res)
     }
 }
